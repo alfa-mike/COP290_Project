637d9f96523e7a04dfa0d8441bb1315c50ebf74f
souvagya-ranjan <cs1190405@iitd.ac.in>
2021-07-28 23:27:31 +0530

    Create Guard.h

---
+++ game/Guard.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "GameObject.h"
+#include "Room.h"
+
+#define IMG_GUARD "res/Guard.png"
+class MazeGuard :
+	public GameObject
+{
+private:
+	static SDL_Texture * guardTexture;
+	std::shared_ptr<Room> guardCenterRoomPtr;
+	std::shared_ptr<Room> guardCurRoomPtr;
+	std::vector<std::shared_ptr<Room>>::iterator guardIter;
+	
+public:
+	
+	MazeGuard(std::shared_ptr<Room> setRoom);
+	~MazeGuard();
+
+	//Updates Guard Location Each Cycle
+	void NextCycle()
+	{
+		//Find current guard room
+		auto roomTypesIter = std::find(begin((*guardIter)->roomTypes), end((*guardIter)->roomTypes), Guard);
+		
+		//Remove the "Guard" type from the roomTypes
+		if (roomTypesIter != end((*guardIter)->roomTypes))
+			(*guardIter)->roomTypes.erase(roomTypesIter);
+		
+		//Move guard to next spot
+		std::advance(guardIter, 1);
+		if (guardIter == std::end(guardCenterRoomPtr->connectRooms))
+			guardIter = std::begin(guardCenterRoomPtr->connectRooms);
+		
+		//Change that room type to have "Guard"
+		(*guardIter)->roomTypes.push_back(Guard);
+
+		SetObjRoom(*guardIter);
+	}
+
+};
